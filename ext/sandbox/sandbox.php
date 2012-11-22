9cfde46eb1cb05459b4959ad6aaf5675f5234c37
Bojie Li <bojieli@gmail.com>
2012-11-23 01:17:55 +0800

    sandbox php test create database

--- ext/sandbox/sandbox.php
+++ ext/sandbox/sandbox.php
@@ -10,12 +10,8 @@ echo "Functions available in the test extension:$br\n";
 foreach($functions as $func) {
     echo $func."$br\n";
 }
-echo "$br\n";
-$function = 'confirm_' . $module . '_compiled';
-if (extension_loaded($module)) {
-	$str = $function($module);
-} else {
-	$str = "Module $module is not compiled into PHP";
-}
-echo "$str\n";
+
+echo "my appid: ".get_appid()."\n";
+echo "created appid: ".create_app('appname1', 'username1', 'email1', 'password1')."\n";
+mysql_query("CREATE TABLE test (id INT(10))");
 ?>
