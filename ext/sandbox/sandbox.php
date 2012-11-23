ad34f3055d85e597412475f27f986bf61408c888
Bojie Li <bojieli@gmail.com>
2012-11-24 04:31:09 +0800

    a stronger sandbox test php

--- ext/sandbox/sandbox.php
+++ ext/sandbox/sandbox.php
@@ -13,7 +13,14 @@ foreach($functions as $func) {
 
 echo "my appid: ".get_appid()."\n";
 $rand = random_string(5);
-$appid = create_app('appname'.$rand, 'username'.$rand, 'email'.$rand, 'password1');
+include "verify.inc.php";
+$appname = 'appname'.$rand;
+$email = 'boj'.$rand.'@mail.ustc.edu.cn';
+$username = 'user'.$rand;
+var_dump(checkname($appname));
+var_dump(checkemail($email));
+var_dump(checkfolder($username));
+$appid = create_app($appname, $username, $email, 'password1');
 echo "created appid: $appid\n";
 mysql_query("CREATE TABLE test (id INT(10))");
 var_dump(get_appinfo($appid));
