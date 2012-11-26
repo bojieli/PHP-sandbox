bbfb2ab37648d4e87a803b6e78802d290a98be23
Bojie Li <bojieli@gmail.com>
2012-11-26 20:00:06 +0800

    enable debug and modify buildconf

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-./buildconf --force
-./configure --disable-phar --enable-fpm --with-mcrypt --with-zlib --enable-mbstring --disable-pdo --with-mysql --with-mysqli --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir --disable-debug --disable-rpath --enable-inline-optimization --with-zlib --enable-mbregex --with-mhash --enable-zip --with-pcre-regex --enable-soap --enable-daemon --enable-sandbox --enable-blog
+./buildconf
+./configure --disable-phar --enable-fpm --with-mcrypt --with-zlib --enable-mbstring --disable-pdo --with-mysql --with-mysqli --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir --disable-rpath --enable-inline-optimization --with-zlib --enable-mbregex --with-mhash --enable-zip --with-pcre-regex --enable-soap --enable-daemon --enable-sandbox --enable-blog --enable-debug
 make -j5
