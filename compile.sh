3424b6e7b22c334c6852b415af82e2dab5fdd65a
Bojie Li <bojieli@gmail.com>
2012-11-26 20:56:05 +0800

    disable debug because there are too many memory leaks

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 ./buildconf
-./configure --disable-phar --enable-fpm --with-mcrypt --with-zlib --enable-mbstring --disable-pdo --with-mysql --with-mysqli --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir --disable-rpath --enable-inline-optimization --with-zlib --enable-mbregex --with-mhash --enable-zip --with-pcre-regex --enable-soap --enable-daemon --enable-sandbox --enable-blog --enable-debug
+./configure --disable-phar --enable-fpm --with-mcrypt --with-zlib --enable-mbstring --disable-pdo --with-mysql --with-mysqli --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir --disable-rpath --enable-inline-optimization --with-zlib --enable-mbregex --with-mhash --enable-zip --with-pcre-regex --enable-soap --enable-daemon --enable-sandbox --enable-blog --disable-debug
 make -j5
