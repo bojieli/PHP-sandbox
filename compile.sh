05d2cdb28b8501c96b5e16f557334018395c62df
Bojie Li <bojieli@gmail.com>
2014-08-25 17:24:59 +0800

    disable mysqli

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf autom4te.cache
 ./buildconf
-./configure --disable-phar --enable-fpm --with-openssl --with-mcrypt --with-zlib --enable-mbstring --disable-pdo --with-mysql --with-mysqli --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir --disable-rpath --enable-inline-optimization --with-zlib --enable-mbregex --with-mhash --enable-zip --with-pcre-regex --enable-soap --enable-daemon --enable-sandbox --enable-blog --disable-debug --enable-apc
+./configure --disable-phar --enable-fpm --with-openssl --with-mcrypt --with-zlib --enable-mbstring --disable-pdo --with-mysql --without-mysqli --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir --disable-rpath --enable-inline-optimization --with-zlib --enable-mbregex --with-mhash --enable-zip --with-pcre-regex --enable-soap --enable-daemon --enable-sandbox --enable-blog --disable-debug --enable-apc
 make -j8
