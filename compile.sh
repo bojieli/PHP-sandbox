601158e397e589bb695df0b7bc7eb3b033266a81
Bojie Li <bojieli@gmail.com>
2015-05-31 15:39:06 +0000

    update extension list, disable insecure extensions, add several extensions

--- compile.sh
+++ compile.sh
@@ -1,5 +1,57 @@
 #!/bin/sh
-rm -rf autom4te.cache
+rm -rf autom4te.cache configure Makefile
 ./buildconf
-./configure --disable-phar --enable-fpm --with-openssl --with-mcrypt --with-zlib --enable-mbstring --disable-pdo --with-mysql --without-mysqli --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir --disable-rpath --enable-inline-optimization --with-zlib --enable-mbregex --with-mhash --enable-zip --with-pcre-regex --enable-soap --enable-daemon --enable-sandbox --enable-blog --disable-debug --with-imagick --with-curl --enable-opcache --disable-cli
+./configure \
+    --enable-bcmath \
+    --enable-blog \
+    --with-bz2 \
+    --disable-cgi \
+    --disable-cli \
+    --with-curl \
+    --enable-daemon \
+    --enable-debug \
+    --enable-dom \
+    --enable-exif \
+    --enable-ftp \
+    --enable-fpm \
+    --with-gd \
+    --with-geoip \
+    --with-gnupg \
+    --without-imap \
+    --without-imap-ssl \
+    --with-imagick \
+    --with-jpeg-dir \
+    --without-ldap \
+    --without-ldap-sasl \
+    --enable-mbregex \
+    --enable-mbstring \
+    --with-mcrypt \
+    --enable-memcached \
+    --disable-memcached-sasl \
+    --with-mhash \
+    --with-mysql \
+    --enable-mysqlnd \
+    --with-mysqli \
+    --enable-opcache \
+    --with-openssl \
+    --enable-inline-optimization \
+    --enable-inotify \
+    --with-pdflib \
+    --with-pdo-mysql \
+    --enable-pdo \
+    --without-pear \
+    --disable-phar \
+    --with-png-dir \
+    --disable-posix \
+    --disable-rpath \
+    --with-pcre-regex \
+    --without-snmp \
+    --enable-soap \
+    --enable-sandbox \
+    --with-xmlrpc \
+    --with-xsl \
+    --with-zlib \
+    --with-zlib-dir \
+    --enable-zip
+
 make -j8
